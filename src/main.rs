d4423ff9c02f3aaea0383cc72d0a75bbcb5f0d88
jcoppis <javier.coppis@globant.com>
2022-02-22 17:16:39 -0300

    invaders with arr boundary err when the invaders are at the bottom of the screen

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ use crossterm::{
     terminal::{self, EnterAlternateScreen, LeaveAlternateScreen},
     ExecutableCommand
 };
-use invaders::{frame::{self, new_frame, Drawable}, render, player::Player};
+use invaders::{frame::{self, new_frame, Drawable}, render, player::Player, invaders::Invaders};
 
 // use rusty_audio::Audio;
 
@@ -49,6 +49,7 @@ fn main() -> Result<(), Box<dyn Error>> {
     // Game loop
     let mut player = Player::new();
     let mut instant = Instant::now();
+    let mut invaders = Invaders::new();
     'gameloop: loop {
         // Per-frame init
         let delta = instant.elapsed();
@@ -76,9 +77,15 @@ fn main() -> Result<(), Box<dyn Error>> {
 
         // Updates
         player.update(delta);
+        if invaders.update(delta) {
+            // audio.play("move");
+        }
 
         // Draw & render
-        player.draw(&mut curr_frame);
+        let drawables: Vec<&dyn Drawable> = vec![&player, &invaders];
+        for drawable in drawables {
+            drawable.draw(&mut curr_frame);
+        }
         let _ = render_tx.send(curr_frame);
         thread::sleep(Duration::from_millis(1));
     }
